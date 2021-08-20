a54a76d53a5af0b800324792baf1ba7b7893d4ce
RevanthKumarG <g.revanth1993@gmail.com>
2021-08-20 11:54:57 +0530

    cucumber pom framework

---
+++ Frameworks/CucumberPOMFramework/test-output-thread/report.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "3f751948-c5dd-477c-9238-c2a398708581",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1629440475256,
+    "group": 13,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1629440553392,
+    "className": "passed"
+  },
+  {
+    "id": "ea82578b-1e1b-4b49-a95d-3de3b4442e72",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1629440592571,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1629440626700,
+    "className": "passed"
+  },
+  {
+    "id": "2e23b386-8ada-4092-859b-7c2b0172e1b6",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1629440553438,
+    "group": 13,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1629440593620,
+    "className": "passed"
+  },
+  {
+    "id": "daa9176a-5add-4ba4-bd69-27e3af259af4",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1629440475248,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1629440551097,
+    "className": "passed"
+  },
+  {
+    "id": "c1a23941-f7cf-4a24-8c09-6a07a4a566ca",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1629440551137,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1629440592540,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 13,
+    "content": "Thread[pool-1-thread-1,5,main]"
+  },
+  {
+    "id": 14,
+    "content": "Thread[pool-1-thread-2,5,main]"
+  }
+]);
+});
